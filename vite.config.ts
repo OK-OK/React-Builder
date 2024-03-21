9667e8917233175535e5d6067754b3dc14842c0c
JL_Guan <JL_Guan@yeah.net>
2024-03-21 16:25:08 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,36 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '^/api': {
-        target: 'http://192.168.9.203:8404',
+        target: 'http://127.0.0.1:3000',
         changeOrigin: true,
       },
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: info => {
+          const folderName = info.name.includes('.css') ? 'css' : 'assets'
+          return `${folderName}/[name]-[hash][extname]`
+        },
+        chunkFileNames: info => {
+          const lastPath = info.moduleIds.at(-1)
+          const pathList = lastPath.split('/')
+          return `${
+            pathList.includes('src') ? `modules/${pathList.at(-2)}` : 'vendor'
+          }/[name]-[hash].js`
+        },
+        manualChunks: id => {
+          if (id.includes('node_modules')) {
+            const nameList = id.split('/')
+            const index = nameList.findIndex(n => n === 'node_modules')
+            if (index !== -1) {
+              return nameList[index + 1]
+            }
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
